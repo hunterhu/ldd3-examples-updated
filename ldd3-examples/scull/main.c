c36938e018761b57bdd7c06b8072968de7051c86
Hunter Hu <hunterhu.ca@gmail.com>
2012-12-30 11:32:41 -0600

    Scull main.c fixes for current kernel 3.6.10
    APIs and include files that need to be updated to compile.

--- ldd3-examples/scull/main.c
+++ ldd3-examples/scull/main.c
@@ -14,7 +14,6 @@
  *
  */
 
-#include <linux/config.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/init.h>
@@ -29,7 +28,7 @@
 #include <linux/seq_file.h>
 #include <linux/cdev.h>
 
-#include <asm/system.h>		/* cli(), *_flags */
+#include <asm/switch_to.h>		/* cli(), *_flags */
 #include <asm/uaccess.h>	/* copy_*_user */
 
 #include "scull.h"		/* local definitions */
@@ -553,7 +552,6 @@ struct file_operations scull_fops = {
 	.llseek =   scull_llseek,
 	.read =     scull_read,
 	.write =    scull_write,
-	.ioctl =    scull_ioctl,
 	.open =     scull_open,
 	.release =  scull_release,
 };
@@ -649,7 +647,11 @@ int scull_init_module(void)
 	for (i = 0; i < scull_nr_devs; i++) {
 		scull_devices[i].quantum = scull_quantum;
 		scull_devices[i].qset = scull_qset;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 37)
+        sema_init(&scull_devices[i].sem, 1);
+#else
 		init_MUTEX(&scull_devices[i].sem);
+#endif
 		scull_setup_cdev(&scull_devices[i], i);
 	}
 
